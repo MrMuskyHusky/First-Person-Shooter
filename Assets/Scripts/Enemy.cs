c6ac5aae698c81273b5a5dc21adc912168fca661
Ethan McGimpsey <54158496+MrMuskyHusky@users.noreply.github.com>
2020-03-10 16:23:54 +1100

    Update #1

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health;
+    void SetKinematic(bool newValue)
+    {
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies)
+        {
+            rb.isKinematic = newValue;
+        }
+    }
+    void Start()
+    {
+        SetKinematic(true);
+    }
+    public void Damage(float amount)
+    {
+        health -= amount;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        SetKinematic(false);
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationalAxis
+    {
+        MouseX,
+        MouseY,
+    }
+
+    [Header("Rotation Variables")]
+    public RotationalAxis axis = RotationalAxis.MouseX;
+    [Range(0, 200)]
+    public float sensitivity = 100;
+    public float minY = -60, maxY = 60;
+    private float _rotY;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+        if (GetComponent<Camera>())
+        {
+            axis = RotationalAxis.MouseY;
+        }
+    }
+    void Update()
+    {
+        if (axis == RotationalAxis.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime, 0);
+        }
+        else
+        {
+            _rotY += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+            _rotY = Mathf.Clamp(_rotY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-_rotY, 0, 0);
+        }
+    }
+}   
